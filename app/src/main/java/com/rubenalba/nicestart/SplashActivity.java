53e63d967227707cedaaf82e0d91c3e88de3e19d
Ruben-am <ru.alba.dev@gmail.com>
2025-11-11 13:50:19 +0100

    feat: add start method in SplasActivity

--- app/src/main/java/com/rubenalba/nicestart/SplashActivity.java
+++ app/src/main/java/com/rubenalba/nicestart/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.rubenalba.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,23 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        openMain();
+    }
+
+    private void openMain() {
+        Handler handler = new Handler(android.os.Looper.getMainLooper());
+        handler.postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 5000
+        );
     }
 }
