9050bf6c757d24c48068e88d2ffa48789745c0cb
Ruben-am <ru.alba.dev@gmail.com>
2025-11-20 08:52:50 +0100

    feat: add context menu to mainActivity

--- app/src/main/java/com/rubenalba/nicestart/MainActivity.java
+++ app/src/main/java/com/rubenalba/nicestart/MainActivity.java
@@ -1,6 +1,12 @@
 package com.rubenalba.nicestart;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +20,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mycontext = findViewById(R.id.mycontext);
+        registerForContextMenu(mycontext);
+    }
+
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.item1) {
+            Toast toast = Toast.makeText(this, "Item copied",
+                    Toast.LENGTH_LONG);
+            toast.show();
+        } else if (item.getItemId() == R.id.item2) {
+            Toast toast2 = Toast.makeText(this, "Downloading item...",
+                    Toast.LENGTH_LONG);
+            toast2.show();
+        }
+        return false;
     }
 }
